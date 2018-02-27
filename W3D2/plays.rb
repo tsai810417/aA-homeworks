fd85b13611f2b59073ebe1be686e57be51cfee09
tsai810417 <tsai_cheng@yahoo.com>
2018-02-26 22:01:15 -0800

    Finish plays.rb

--- W3D2/plays.rb
+++ W3D2/plays.rb
@@ -14,6 +14,14 @@ end
 class Play
   attr_accessor :title, :year, :playwright_id
 
+  def self.find_by_title(title)
+
+  end
+
+  def self.find_by_playwright(name)
+
+  end
+
   def self.all
     data = PlayDBConnection.instance.execute("SELECT * FROM plays")
     data.map { |datum| Play.new(datum) }
@@ -44,6 +52,48 @@ class Play
         plays
       SET
         title = ?, year = ?, playwright_id = ?
+      WHERE
+        id = ?
+    SQL
+  end
+end
+
+class Playwright
+  attr_accessor :title, :year, :playwright_id
+
+  def self.find_by_name(name)
+
+  end
+
+  def self.all
+    data = PlaywrightDBConnection.instance.execute("SELECT * FROM playwright")
+    data.map { |datum| Playwright.new(datum) }
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @name = options['name']
+    @birth_year = options['birth_year']
+  end
+
+  def create
+    raise "#{self} already in database" if @id
+    PlaywrightDBConnection.instance.execute(<<-SQL, @name, @birth_year)
+      INSERT INTO
+        plays (name, birth_year)
+      VALUES
+        (?, ?, ?)
+    SQL
+    @id = PlaywrightDBConnection.instance.last_insert_row_id
+  end
+
+  def update
+    raise "#{self} not in database" unless @id
+    PlaywrightDBConnection.instance.execute(<<-SQL, @name, @birth_year, @id)
+      UPDATE
+        plays
+      SET
+        name = ?, birth_year = ?
       WHERE
         id = ?
     SQL
